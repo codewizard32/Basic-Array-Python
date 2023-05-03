c6c88a9b473f83bc953e42dfa5649bfcfe7b4495
codewizard32 <131467398+codewizard32@users.noreply.github.com>
2023-05-03 12:16:57 +0530

    Update array_reversal.py

--- array_reversal.py
+++ array_reversal.py
@@ -10,3 +10,15 @@ for i in range(num//2):
 print("Reverse all elements of the array:")
 for i in range(num):
     print(arr[i], end=" ")
+
+'''
+Enter the total number of elements: 6
+Enter the element 1: 1
+Enter the element 2: 2
+Enter the element 3: 3
+Enter the element 4: 4
+Enter the element 5: 5
+Enter the element 6: 6
+Reverse all elements of the array:
+6 5 4 3 2 1
+'''
